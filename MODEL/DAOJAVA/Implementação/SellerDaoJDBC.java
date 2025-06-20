72e5ef7d3c8cebad9ad96d98626b5e1029fba6d0
MILENARIBEIRO160 <133072503+MILENARIBEIRO160@users.noreply.github.com>
2025-06-20 16:15:21 -0300

    Add files via upload

---
+++ MODEL/DAOJAVA/Implementação/SellerDaoJDBC.java
@@ -0,0 +1,41 @@
+package model.dao.imbl;
+
+import java.util.List;
+
+import model.dao.Interger;
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+public class SellerDaoJDBC implements SellerDao{
+
+	@Override
+	public void insert(Seller obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Seller obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Interger id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Seller findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Seller> findALL() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
